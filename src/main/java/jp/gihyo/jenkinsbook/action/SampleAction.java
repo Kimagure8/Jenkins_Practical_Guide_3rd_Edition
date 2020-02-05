391f9b1bb84631110941da70afec0195e50d6149
Yuzuru Seto <lostchild.wolf@gmail.com>
2020-02-05 16:48:41 +0900

    Fix etc.

--- src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
+++ src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
@@ -62,12 +62,12 @@ public class SampleAction {
      */
     public final String execute(final HttpServletRequest request) {
         SampleDTO dto = new SampleDTO(firstName, lastName);
-        private final int AFTERNOON = 12;        
+        private final int number = 12;
 
         //Select the greeting message according to the time
         Calendar calendar = Calendar.getlnstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        if (hour < AFTERNOON) {
+        if (hour < number) {
           dto.setMessage("Good morning");
         } else {
           dto.setMessage("Good afternoon");
